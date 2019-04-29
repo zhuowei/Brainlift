be12351ac6e967c47041cf30002762cdc3747039
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-04-28 23:56:58 -0700

    Create a function id for main

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-use cranelift_codegen::isa;
-use cranelift_codegen::settings;
+use cranelift_codegen::ir::{Signature, Type};
+use cranelift_codegen::isa::{self, CallConv};
+use cranelift_codegen::settings::{self, Configurable};
 use cranelift_faerie::{FaerieBackend, FaerieBuilder, FaerieTrapCollection};
-use cranelift_module::{Module, ModuleResult};
+use cranelift_module::{Linkage, Module, ModuleResult};
 use std::str::FromStr;
 use target_lexicon::triple;
 fn main() {
-    println!("Hello, world!");
     compile().unwrap();
 }
 
+// note: the return type really should have a better error type!
 fn compile() -> ModuleResult<()> {
     // first, we construct a TargetIsa to tell Cranelift to emit code for the x86_64 cpu on my computer.
     // https://docs.rs/cranelift-codegen/0.30.0/cranelift_codegen/isa/index.html
-    let shared_builder = settings::builder();
+    let mut shared_builder = settings::builder();
+    shared_builder.enable("is_pic");
     let shared_flags = settings::Flags::new(shared_builder);
-    let isa = isa::lookup(triple!("x86_64-apple-darwin"))
-        .unwrap()
-        .finish(shared_flags);
+    let mut isa_builder = isa::lookup(triple!("x86_64-apple-darwin")).unwrap();
+    let isa = isa_builder.finish(shared_flags);
     // To emit some code, we need a Backend to write the code to an object file
     // so we can link it into an executable.
     // we use the cranelift_faerie crate for this.
@@ -26,6 +27,10 @@ fn compile() -> ModuleResult<()> {
         FaerieTrapCollection::Disabled,
         FaerieBuilder::default_libcall_names(),
     )?;
-    let module: Module<FaerieBackend> = Module::new(backend_builder);
+    let mut module: Module<FaerieBackend> = Module::new(backend_builder);
+    // define our main function
+    // note: this gives void main(), which is wrong but it still works.
+    let mut signature = Signature::new(CallConv::SystemV);
+    let function_id = module.declare_function("main", Linkage::Export, &signature)?;
     Ok(())
 }
